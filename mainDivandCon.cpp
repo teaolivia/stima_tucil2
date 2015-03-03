30a04f1ada36852507d0cfe9470833fae76a1933
Thea Olivia <theaolivia.mail@gmail.com>
2015-03-04 01:33:49 +0700

    Create mainDivandCon.cpp

---
+++ mainDivandCon.cpp
@@ -0,0 +1,9 @@
+#include "DivandCon.h"
+
+DivandCon::DivandCon(){
+	cout << "Masukkan nilai n =  \n",&n,%d << endl;
+	DivandCon SelectSort;
+	cout << "Jarak-jarak antar-titik = \n" << endl;
+};
+
+DivandCon::~DivandCon(){};
