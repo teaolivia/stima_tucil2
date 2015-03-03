b56da96e9a96d5cbf252b5f8aaf688b6add4c728
Thea Olivia <theaolivia.mail@gmail.com>
2015-03-04 01:33:04 +0700

    Update DivandCon.h

--- DivandCon.h
+++ DivandCon.h
@@ -1,1 +1,22 @@
 
+#include "Point3d.h"
+#include <cstdio>
+#include <iostream>
+
+
+using namespace std;
+
+class DivandCon(){
+	public:
+	// atribut
+	int n; // indeks maksimal dalam tabel, ukuran array
+	int i; // untuk iterasi
+	float D[]; // array kosong untuk menyimpan hasil perhitungan Euclidean untuk di-sort
+
+	
+	// method
+	void SelectSort(int D[], int left, int right){}; // memilih anggota yang memiliki nilai terkecil, implementasi Algoritma Divide and Conquer
+	void PrintArray(int D[], int n){}; // mencetak banyak Eucledian yang dipakai
+	
+	
+};
